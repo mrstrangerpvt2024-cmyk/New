cfcbd331bf3986d678f89005121200cce8c9aff9
mrstrangerpvt2024-cmyk <mrstrangerpvt2024@gmail.com>
2025-11-13 16:18:36 +0530

    Update main.py

--- main.py
+++ main.py
@@ -16,7 +16,6 @@ import cloudscraper
 from Crypto.Cipher import AES
 from Crypto.Util.Padding import unpad
 from base64 import b64encode, b64decode
-from logs import logging
 from bs4 import BeautifulSoup
 import saini as helper
 from utils import progress_bar
